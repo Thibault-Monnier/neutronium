899dbf1c193301884e69f3067500f37662c3320b
Thibault-Monnier <thibaultmonni@gmail.com>
2025-10-30 14:28:44 +0100

    Make timer start at beginning of main

--- src/driver/main.cpp
+++ src/driver/main.cpp
@@ -129,9 +129,9 @@ void compileFile(const CompilerOptions& opts, SourceManager& sourceManager, bool
 }  // namespace
 
 int main(const int argc, const char** argv) {
-    const CompilerOptions opts = parse_cli(argc, argv);
-
     const auto startTime = Clock::now();
+
+    const CompilerOptions opts = parse_cli(argc, argv);
 
     runOrDie("rm -rf neutro && mkdir neutro");
 
