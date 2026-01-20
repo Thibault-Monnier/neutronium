b0284a41a8ea967c1fd66c93eec533876fb4e08f
Thibault-Monnier <thibaultmonni@gmail.com>
2026-01-20 18:07:20 +0100

    Allow passing build type directly for config + update workflows

--- configure-cmake.sh
+++ configure-cmake.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-cmake -B build -G Ninja "$@" .
+build_type=""
+if [[ $# -ge 1 ]]; then
+    case "$1" in
+        Debug|Release|RelWithDebInfo)
+            build_type="-DCMAKE_BUILD_TYPE=$1"
+            shift
+            ;;
+    esac
+fi
+
+cmake -B build -G Ninja \
+    ${build_type:+$build_type} \
+    "$@" \
+    .
